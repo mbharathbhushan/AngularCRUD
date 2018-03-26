ffab04dcf23c9958472abfe8161fd5c81b63620f
Bharath Bhushan M <mbharath1983@gmail.com>
2018-03-26 18:30:39 +0530

    Angular CRUD1
    Angular CRUD1

---
+++ AngularCRUD1/src/app/employee_model.ts
@@ -0,0 +1,13 @@
+export class Employee{
+    id:number;
+    name:string;
+    gender:string;
+    email?:string;
+    phoneNumber?:string;
+    contactPreference:string;
+    dateOfBirth:Date;
+    department:string;
+    isActive:string;
+    photoPath:string;
+
+}
